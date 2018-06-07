f71239fde1ffb7c5db0eb83464e81a39d5c0e053
Ryan Carr <rycarr@gmail.com>
2018-06-07 11:15:30 -0500

    Initial commit of Section 4 Part 34 - Favorite Number Challenge

---
+++ Section4.34/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int favorite_number;
+
+    cout << "What is your favorite number? ";
+    cin >> favorite_number;
+
+    cout << "Amazing!! That is my favorite number too!" << endl;
+    cout << "No Really!! " << favorite_number << " is my favorite number!" << endl;
+    return 0;
+}
